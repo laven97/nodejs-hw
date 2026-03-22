1adb1cf43a8dfaed6ebc17a54efd77b670cfb18d
laven97 <poleschucke@gmail.com>
2026-03-22 16:41:05 +0100

    hw1

---
+++ src/service.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import pinoHttp from "pino-http";
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+const logger = pinoHttp({
+  transport: {
+    target: "pino-pretty",
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp())
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({ message: "Retrieved all notes" });
+});
+
+app.get("/notes/:noteId", (req, res) => {
+    const {noteId} = req.params
+
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.get("/test-error", () => {
+  throw new Error("Simulated server error");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
