9dc8b467e8d5b9b64e007c26505f01b33718bebb
laven97 <poleschucke@gmail.com>
2026-03-25 20:34:31 +0100

    hw2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -15,7 +15,7 @@ export const getNoteById = async (req, res) => {
     throw createHttpError(404, 'Note not found');
   }
 
-  res.status(200).json(noteId);
+  res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
